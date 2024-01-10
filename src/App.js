541007d7fb721cb7ce791b9e38d20ade94bf0a23
Erik Cupsa <2003eec@gmail.com>
2024-01-10 17:28:35 -0500

    Finished work on nav bar

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Dev from './components/About/Dev';
 
 function App() {
   useEffect(() => {
-    document.title = 'Erik\'s Portfolio';
+    document.title = 'Max\'s Portfolio';
   }, []);
 
   return (
