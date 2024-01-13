b3f9d4d10d3a09eadf769ce0024ae9cfdd13fa7b
Erik Cupsa <2003eec@gmail.com>
2024-01-13 00:01:59 -0500

    Completed work experience page

--- src/components/Work/Timeline/index.js
+++ src/components/Work/Timeline/index.js
@@ -5,14 +5,37 @@ const Timeline = () => {
   const [events, setEvents] = useState([
     {
       date: 'May-September 2023',
-      title: 'TRC Consulting, Inc.',
+      title: "Kadama Tutoring",
       description: [
-        'Designed <b>30+ AWS Lambda Functions in AWS</b>, strategically leveraging <b>Lambda layers</b> to increase processing speed by <b>35% </b>and re-usability by <b>52%</b>, resulting in cost savings of <b>$1,000</b> per large scale deployment',
-        'Implemented <b>15+</b> serverless workflows using <b>AWS Step Functions</b>, seamlessly integrating <b>REST APIs </b>and rigorously tested with <b>Postman</b>, resulting in a <b>65%</b> reduction in execution time and enhanced operational efficiency',
-        'Crafted a <b>Spring Boot</b> application with a <b>Drools</b> logic engine, containerized it using <b>Docker</b>, and seamlessly deployed it on <b>AWS</b> using <b>EC2</b>, strategically allocating cloud resources to ensure enhanced scalability and reliability',
+        "Dedicated and results-driven tutor with a proven track record of success in guiding students through challenging subjects.",
+        "Specialized in providing comprehensive support in <b>geometry, trigonometry, math analysis, calculus 1, physics, intro to computer science, and SAT preparation</b>.",
+        "Over the course of a year, successfully assisted more than <b>30 students</b> in understanding and mastering these subjects, fostering a deep appreciation for learning and academic achievement.",
       ],
-      expanded: false,
+      "expanded": false
+    },
+    {
+      date: "October 2022-January 2024",
+      title: "NASA HUNCH",
+      description: [
+        "Spearheaded the development of <b>magnetic boots</b> by leveraging expertise in manipulating stepper motors and electromagnets using <b>Arduino</b> and <b>C++</b>.",
+        "Led the design and implementation of a fully automated system, showcasing technical proficiency and attention to detail.",
+        "Achieved notable recognition by emerging as one of the top four teams out of 800 participating groups in the <b>magnetic boots challenge</b>."
+      ],
+      "expanded": false
+    }, 
+    {
+      date: "September 2022-June 2023",
+      title: "Hack Club",
+      description: [
+        "Pioneering leader in the establishment and leadership of the first Computer Science Club at <b>Chatfield Senior High School</b>, operating under the auspices of the <b>Hack Club</b> organization.",
+        "Initiated and cultivated a vibrant community, guiding a diverse group of over <b>20 students</b> in their introduction to coding.",
+        "Delivered engaging sessions to teach foundational coding concepts in <b>Java</b>, fostering a passion for computer science among club members.",
+        "Demonstrated strong leadership skills by leading a subgroup of <b>five students</b> to participate in a <b>hackathon</b>, where the team showcased their coding skills and problem-solving capabilities."
+      ],
+      "expanded": false
     }
+    
+    
   ]);
 
   const handleItemClick = (index) => {
